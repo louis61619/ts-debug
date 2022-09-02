97d7f429141fd02b99a9bd50c3b05c09f7f8ca5c
louis61619 <louis61619@gmail.com>
2022-09-02 13:30:04 +0800

    init

---
+++ ts-script/dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+function sum(x, y) {
+    let a = x * 2;
+    let b = y * 100;
+    let c = a + b;
+    return x + y + a + b + c;
+}
+sum(1, 3334);
+//# sourceMappingURL=index.js.map
---
+++ ts-script/index.ts
@@ -0,0 +1,12 @@
+function sum(x: number, y: number) {
+  
+  let a = x * 2
+
+  let b = y * 100
+
+  let c = a + b
+
+  return x + y + a +b + c
+}
+
+sum(1, 3334)
